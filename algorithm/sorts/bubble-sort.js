50af21b2e57dd7dadc2f75ef1732b2160d9da13d
grady <grady0902@gmail.com>
2025-06-30 23:43:29 +0800

    feat: bubble sort

--- algorithm/sorts/bubble-sort.js
+++ algorithm/sorts/bubble-sort.js
@@ -2,18 +2,16 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
     //Code here
-    // return array.sort((a, b) => {
-    //     if (a > b) {
-    //         return a - b;
-    //     } else if (a < b) {
-    //         return b - a;
-    //     } else {
-    //         return 0;
-    //     }
-    // });
-    let index = 0;
-    for (let n of array) {
-        
+
+    for (let n = array.length - 1; n > 0; n--) {
+        for (let i = 0; i <= n; i++) {
+            const a = array[i];
+            const b = array[i +1];
+            if (a > b) {
+                array[i] = b;
+                array[i + 1] = a;
+            }
+        }
     }
 }
 
